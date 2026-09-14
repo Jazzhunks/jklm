7c9847421b973bd37bf772e56beaf1b9cd1352ee
Mudasir Mushtaq <mudasirmushtaq@Mudasirs-MacBook-Pro.local>
2026-09-14 12:13:31 +0530

    fix: remove legacy adminData destructuring in SchoolVisitsTab

--- frontend/src/pages/admin/tabs/SchoolVisitsTab.jsx
+++ frontend/src/pages/admin/tabs/SchoolVisitsTab.jsx
@@ -1,13 +1,8 @@
 import React from "react";
+import AdminSchoolVisits from "@/pages/AdminSchoolVisits";
 
-const SchoolVisitsTab = React.memo(({ adminData, innerSearch, schKind }) => {
-  const { summary, enrollments, scholarshipApps, jobApps, inquiries, courses, notices, jobs, centers, testimonials, results, gallery, adminCampaigns, posts, loadingData, load, filteredEnrollments, filteredScholarships, filteredJobApps, filteredCourses, filteredNotices, filteredJobs, filteredCenters, filteredTestimonials, filteredResults, filteredCampaigns, filteredInquiries, filteredGallery, galleryCategories, filteredPosts, updateStatus, post, del, setFeatured, clearFeatured } = adminData;
-
-  return (
-    <>
-      <AdminSchoolVisits />
-    </>
-  );
+const SchoolVisitsTab = React.memo(() => {
+  return <AdminSchoolVisits />;
 });
 
 export default SchoolVisitsTab;
